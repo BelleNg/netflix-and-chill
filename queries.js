79773517414b5f34f03fd6bf53b2e58cec123df4
Belle Nguyen <NgBelleK@gmail.com>
2020-12-01 17:14:04 -0600

    connected database successfully

--- queries.js
+++ queries.js
@@ -6,3 +6,16 @@ const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT
   });
+
+function authenticate() {
+    sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+}
+
+module.exports = { authenticate };
--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 require('dotenv').config({path: path.resolve(__dirname+'/.env')});
+const queries = require('./queries')
 
 
 app.use(express.static(__dirname + '/public'));
  
 app.get('/', (req, res) => {
  res.send({working: true})
+ queries.authenticate();
 })
  
 app.listen(process.env.PORT, () => {
